3d51c81bceafde95a7b749c4c8ae416ad799e652
Soo-kyeongLee <21900522@handong.edu>
2021-09-07 02:41:06 +0900

    dd

---
+++ src/com/example/lab1/main.java
@@ -0,0 +1,16 @@
+package com.example.lab1;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		System.out.println("hello world!\n");
+		System.out.print("name:");
+		String name=s.next();
+		System.out.println("["+name+"]\n");
+	}
+
+}
